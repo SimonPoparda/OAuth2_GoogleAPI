076699d376dab8bac9495185e12a66665e1a7bff
“Simon <szymonpoparda@wp.pl>
2025-02-26 10:13:31 +0100

    Added new

--- Application_v2/app.py
+++ Application_v2/app.py
@@ -6,6 +6,8 @@ import creds
 SCOPES = creds.SCOPES
 SERVICE_ACCOUNT_FILE = creds.SERVICE_ACCOUNT_FILE
 PARENT_FOLDER_ID = creds.PARENT_FOLDER_ID
+file_ids = creds.file_ids
+file_names = creds.file_names
 
 def authenticate():
     creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
@@ -25,4 +27,16 @@ def upload_photo(file_path):
         media_body=file_path,
     ).execute()
 
+def download_photos():
+    creds = authenticate()
+    service = build('drive', 'v3', credentials=creds)
+
+    for file_id, file_name in zip(file_ids, file_names):
+        request = service.files().get_media(fileId=file_id)
+        with open(file_name, 'wb') as file:
+            file.write(request.execute())
+        print(f"Downloaded: {file_name}")
+
 upload_photo('Blue_Nature.jpg')
+download_photos()
+
